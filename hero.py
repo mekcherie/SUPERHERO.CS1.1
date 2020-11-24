68bc238b6e53760c0c8577c70468bf309d486139
Meklit Cherie <makywonde347@yahoo.com>
2020-11-23 21:15:18 -0800

    completed step 2

--- hero.py
+++ hero.py
@@ -1,6 +1,35 @@
+import random
+
 class Hero:
     # we want our hero to have a default "starting_health"
-    def__init__(self, name, starting_health=100)
-    self.name= name 
-    self.starting_health = starting_health
-    self.current_health = starting_health
+    # so we can set that in the function header.
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+            name: String
+            starting_health: Integer
+            current_health: Integer
+        '''
+        self.name= name 
+        self.starting_health = starting_health
+        self.current_health = starting_health
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
