82a1ba35a59e9e1f10d886653db975b523ea6376
Meklit Cherie <makywonde347@yahoo.com>
2020-11-28 13:41:44 -0800

    creating files

---
+++ ability.py
@@ -0,0 +1,14 @@
+import random
+
+class Ability:
+    def __init__(self, name, max_damage):
+         '''
+         Initialize the values passed into this method as instance variables.
+         '''
+         self.name = name
+         self.max_damage = max_damage
+
+    def attack(self):
+         ''' Return a value between 0 and the value set by self.max_damage.'''
+         random_value = random.randint(0,int(self.max_damage))
+         return random_value
--- hero.py
+++ hero.py
@@ -1,20 +1,37 @@
-import random
+from ability import Ability
+from armor import Armor
 
-class Hero:
-    # we want our hero to have a default "starting_health"
+class Hero: 
+     # We want our hero to have a default "starting_health",
     # so we can set that in the function header.
     def __init__(self, name, starting_health=100):
-        '''Instance properties:
-            name: String
-            starting_health: Integer
-            current_health: Integer
-        '''
-        self.name= name 
+      '''Instance properties:
+          abilities: List
+          armors: List
+          name: String
+          starting_health: Integer
+          current_health: Integer
+      '''
+
+        # abilities and armors don't have starting values,
+        # and are set to empty lists on initialization
+        self.abilities = list()
+        self.armors = list()
+        # we know the name of our hero, so we assign it here
+        self.name = name
+        # similarly, our starting health is passed in, just like name
         self.starting_health = starting_health
+        # when a hero is created, their current health is
+        # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
 
 
 
+    def add_ability(self, ability):
+    ''' Add ability to abilities list '''
+
+    # We use the append method to add ability objects to our list.
+    self.abilities.append(ability)
 
 
 
@@ -25,11 +42,9 @@ class Hero:
 
 
 
-
-
-if __name__ == "__main__":
+    if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    my_hero = Hero("Grace Hopper", 200)
+        my_hero = Hero("Grace Hopper", 200)
     print(my_hero.name)
     print(my_hero.current_health)
