a7ec0c3ebb9bfbab5e79fca0d061637871b8da64
Meklit Cherie <makywonde347@yahoo.com>
2020-11-28 15:07:12 -0800

    done

--- animal.py
+++ animal.py
@@ -1,3 +1,4 @@
+
 class Animal:
   def __init__(self, name):
     self.name = name
@@ -11,3 +12,35 @@ class Animal:
 class Frog(Animal):
     def jump(self):
         print('{} is jumping'.format(self.name))
+
+
+# "EXAMPLE"
+# class Animal:
+#     def __init__(self, name, sleep_time):
+#         self.name = name
+#         self.sleep_time = sleep_time
+
+#     def sleep(self):
+#         print(
+#             "{} sleeps for {} hours {}".format(
+#                 self.name,
+#                 self.sleep_time)
+#     dog = Animal("Sophie", 12)
+#     dog.sleep()
+
+
+# class Animal:
+#     def __init__(self, name, sleep_duration):
+#         self.name = name
+#         self.sleep_duration = sleep_duration
+
+#     def sleep(self):
+#         print(
+#             "{} sleeps for {} hours".format(
+#                 self.name,
+#                 self.sleep_duration))
+
+# # Note that the class Dog takes in Animal as a parameter!
+# class Dog(Animal):
+#     def bark(self):
+#         print("Woof! Woof!")
